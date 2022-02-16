43592e0b3485cae9c766295fa0ee43a13a3de38a
Washington Obungu <tobungu@gmail.com>
2022-02-16 04:24:34 -0800

    Added a condition for no left and right child

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	binary_tree_t *right, *left;
 	size_t height = 0;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (height);
 
 	left = tree->left;
